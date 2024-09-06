e8f7edb6b12cb7c888845b6fd5f007f9268f21a0
Devsstudent <devsstudent777@gmail.com>
2024-09-06 21:24:05 +0200

    malloc.h new version

--- include/malloc.h
+++ include/malloc.h
@@ -21,39 +21,34 @@ typedef enum state {
 
 typedef struct chunk {
 	size_t				size;
-	size_t				alignement;
 	t_state				state;
 	t_type				page_type;
-	struct chunk		*next ;
-	//Prev ?
+	struct chunk		*next;
+	struct chunk		*prev;
 }	t_chunk;
 
-typedef struct pages {
-	size_t		free_chunks;
-	size_t		busy_chunks;
-	size_t		total_bytes_busy;
-	size_t		total_bytes_free;
-	size_t		total_bytes_alignement;
-	t_chunk		*chunks;
-	t_chunk		*last;
-	size_t		page_nb;
-	t_type		type;
-}	t_pages;
+typedef struct mem_zone {
+	size_t			free_chunks;
+	size_t			busy_chunks;
+	struct mem_zone	*next;
+	t_chunk			*first;
+	t_type			page_type;
+	size_t			max_size_available;
+}	t_mem_zone;
 
 typedef struct alloc_info {
 	// < 560
-	t_pages			tiny;
+	t_mem_zone		*tiny;
 	// < 4096
-	t_pages			small;
+	t_mem_zone		*small;
 	// > 4096
-	t_pages			large;
+	t_mem_zone		*large;
 }	t_alloc_info;
-
 
 extern t_alloc_info *get_info(void);
 void	*ft_malloc(size_t size);
-void *ft_realloc(void *ptr, size_t size);
-void ft_free(void *ptr);
+void	*ft_realloc(void *ptr, size_t size);
+void	ft_free(void *ptr);
 void	insert_free_chunk_in_between(size_t free_space, void *addr, t_pages *current_page, t_chunk *base_chunk);
 extern t_chunk *looking_for_chunk(t_pages *page, size_t size);
 extern t_chunk *get_chunk(void *ptr, t_pages *ptr_page);
